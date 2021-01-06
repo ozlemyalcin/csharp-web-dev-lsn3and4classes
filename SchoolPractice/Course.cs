46dc974aec49b0b8fea5e062ee1eeff3441b43f3
ozlemyalcin <omersam33@gmail.com>
2021-01-06 16:30:46 -0600

    done

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolPractice
+{
+    public class Course
+    {
+        private List<string> courseName = new List<string>();
+        private Dictionary<string, int> list = new Dictionary<string, int>();
+
+
+        public int studentNumber { get; set; }
+
+        public List<string> CourseName
+        {
+            get { return courseName; }
+            set { courseName = value; }
+
+        }
+
+        public Dictionary<string, int> List
+        {
+            get { return list; }
+            set { list = value; }
+        }
+
+
+    }
+}
--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SchoolPractice
 {
@@ -7,6 +8,52 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student newStudent = new Student();
+            newStudent.Name = "Ozlem";
+            newStudent.StudentId = 1234;
+            newStudent.NumberOfCredits = 1;
+            newStudent.Gpa = 4.0;
+
+            Console.WriteLine($"Student name: {newStudent.Name}\nStudentId:{newStudent.StudentId}\nCredit: {newStudent.NumberOfCredits}\nGPA: {newStudent.Gpa}");
+
+
+
+            Teacher instructor = new Teacher();
+            instructor.FirstName = "Benet";
+            instructor.LastName = "Clark";
+            instructor.Subject = "Web Development";
+            instructor.YearsTeaching = 5;
+
+            Console.WriteLine($"Instructor Name: {instructor.FirstName}\nLast Name:{instructor.LastName}\nSubject:{instructor.Subject}\nTeaching Years:{instructor.YearsTeaching}");
+
+
+
+            Course newCourse = new Course();
+            newCourse.studentNumber = 30;
+            newCourse.List.Add("Python", 2);
+            newCourse.List.Add("JavaScript", 12);
+            newCourse.List.Add("C#", 16);
+            newCourse.CourseName.Add("Python");
+            newCourse.CourseName.Add("JavaScript");
+            newCourse.CourseName.Add("C#");
+
+            Console.WriteLine($"This course have {newCourse.studentNumber} student");
+
+            foreach (string courseName in newCourse.CourseName)
+            {
+                Console.WriteLine($"This course have {courseName} classes");
+
+            }
+
+            foreach(KeyValuePair<string, int> person in newCourse.List)
+            {
+                Console.WriteLine($" {person.Value} student chose {person.Key} classes");
+
+            }
+            
+
+
+
         }
     }
 }
